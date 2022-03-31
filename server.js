3666751581089b0d497ea760f1086f29bb496396
Phil Splettstoeszer <stoeszer09@gmail.com>
2022-03-30 20:58:21 -0500

    Trying to make Heroku work

--- server.js
+++ server.js
@@ -18,11 +18,6 @@ mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopolo
 )
 app.use(express.static('public'))
 app.use(express.urlencoded({extended: true}))
-
-// ROUTES
-app.get('/', (req, res) => {
-    res.send('Welcome to an Awesome App about Breads')
-})
   
 // Breads
 const breadsController = require('./controllers/breads_controller.js')
@@ -31,6 +26,11 @@ app.use('/breads', breadsController)
 // bakers
 const bakersController = require('./controllers/bakers_controllers.js')
 app.use('/bakers', bakersController)
+
+// ROUTES
+app.get('/', (req, res) => {
+  res.send('Welcome to an Awesome App about Breads')
+})
 
 // 404 Page
 app.get('*', (req, res) => {
